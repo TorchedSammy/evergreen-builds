0dac44e28953f6f5effa9ae5440be700222e016d
sammyette <torchedsammy@gmail.com>
2024-01-19 05:44:27 -0500

    chore: remove d
    i think that's the one that's causing errors anyway, ci logs are gone

--- setup.sh
+++ setup.sh
@@ -2,15 +2,13 @@
 
 REPOS="https://github.com/tree-sitter/tree-sitter-c
 https://github.com/tree-sitter/tree-sitter-cpp
-https://github.com/CyberShadow/tree-sitter-d
 https://github.com/the-mikedavis/tree-sitter-diff
 https://github.com/tree-sitter/tree-sitter-go
 https://github.com/camdencheek/tree-sitter-go-mod
 https://github.com/tree-sitter/tree-sitter-javascript
 https://github.com/tree-sitter/tree-sitter-julia
 https://github.com/MunifTanjim/tree-sitter-lua
-https://github.com/tree-sitter/tree-sitter-rust
-https://github.com/maxxnino/tree-sitter-zig"
+https://github.com/tree-sitter/tree-sitter-rust"
 
 clone_and_generate() {
     printf '%s' "$REPOS" | xargs -d"\n" -n 1 -P 4 -t git clone --depth 1 --no-tags
